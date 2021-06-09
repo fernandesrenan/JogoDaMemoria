8f711247a547b1af5d801c49cbc34c2e52245e17
Renan Fernandes <renan.fernandes@cwi.com.br>
2021-06-09 20:50:47 -0300

    Initial Commit

---
+++ JogoDaMemoria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JogoDaMemoria
+//
+//  Created by Renan Carvalho de Oliveira Fernandes on 09/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
